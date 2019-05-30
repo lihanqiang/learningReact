1249ff59fcf8f22624402304c62eb578e29dd24f
Jack <1162817095@qq.com>
2019-05-30 15:18:38 +0800

    首次提交代码

---
+++ src/containers/docs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Documentation from '~/components/documentation';
+
+class Docs extends React.Component {
+  state = {
+  };
+  render() {
+    return (
+      <Documentation />
+    );
+  }
+}
+
+export default Docs;
